4012a838f9a8c8363fec03988b6b58172075caca
Yusuke Izawa <me@yizawa.com>
2025-06-15 17:48:30 +0200

    Add test_jit.jl

---
+++ test/test_jit.jl
@@ -0,0 +1,60 @@
+using Test
+
+include("../src/diff.jl")
+include("../src/peval.jl")
+include("../src/jit.jl")
+
+function test()
+    code = """
+function entry()
+    x = 1
+    y = x + 1
+    return y + 2
+end"""
+    res = run(code, "entry")
+    return res == 4
+end
+
+@test test()
+
+function test2()
+        code1 = """
+function f(x, y)
+    if x <= 1
+        x = x + 2
+    end
+    return x + y
+end"""
+
+    code2 = """
+function f(x, y)
+    if x <= 1
+        x = x + 10
+    end
+    return x + y
+end"""
+
+    t1 = expr_to_treenode(Meta.parse(code1))
+    t2 = expr_to_treenode(Meta.parse(code2))
+
+    M = top_down(t1, t2, 2)
+    M = bottom_up(t1, t2, M, 0.5, 100)
+
+    N = []
+    for (t1, t2) in M
+        expr1 = treenode_to_expr(t1)
+        expr2 = treenode_to_expr(t2)
+        push!(N, (expr1, expr2))
+    end
+    env = create_env_from_mapping(N)
+    env[:x] = :(1)
+    # func = create_entry(code2, env)
+    # println("\nCreated function entry")
+    # println(func)
+
+    code_gen(code2, env)
+
+    return true
+end
+
+#@test test2()
