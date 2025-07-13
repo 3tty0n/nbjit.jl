752685e9fadbc9144611d18f2b581bcb04537e85
Yusuke Izawa <me@yizawa.com>
2025-07-13 11:33:09 +0200

    Change to @constant annot

--- src/annotate.jl
+++ src/annotate.jl
@@ -1,13 +1,13 @@
 const VAR_ANNOTATIONS = Dict{Symbol, Any}()
 
-macro annotate(note, stmt)
+macro constant(stmt)
     if stmt.head == :(=) && length(stmt.args) == 2
         var = stmt.args[1]
         val = stmt.args[2]
         var_sym = QuoteNode(var)
         quote
             $(esc(var)) = $(esc(val))
-            VAR_ANNOTATIONS[$var_sym] = $(esc(note))
+            VAR_ANNOTATIONS[$var_sym] = $(esc(val))
         end
     else
         error("@annotate can only be used with assignments")
@@ -15,6 +15,8 @@ macro annotate(note, stmt)
 end
 
 function test()
-    @annotate "constant" x = 1
-    return VAR_ANNOTATIONS[:x] == "constant"
+    @constant x = 1
+    return VAR_ANNOTATIONS[:x] == 1
 end
+
+test()
