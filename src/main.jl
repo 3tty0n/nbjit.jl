0bf6c4cce7220edad182b7b77331f5b4ecd0e337
Yusuke Izawa <me@yizawa.com>
2025-07-14 13:36:14 +0200

    Fix main.jl

--- src/main.jl
+++ src/main.jl
@@ -3,15 +3,17 @@ include("./jit.jl")
 
 function main(path)
     # read file
-    f = open(path, 'r')
+    f = open(path, "r")
     s = read(f, String)
     ex = Meta.parse(s)
 
     # partial the source program
-    func_expr, fname = partial_evaluate_and_make_entry(ex)
+    @show func_expr, fname = partial_evaluate_and_make_entry(ex)
 
     # compile code and run
     # TODO: think about the way to integrate with notebook
-    res = compile_and_run(func_expr, fname, 123)
+    res = compile_and_run(func_expr, string(fname), 123)
     println(res)
 end
+
+main(ARGS[1])
