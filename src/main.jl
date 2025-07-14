37defcd5418462b54783d69461210f6c84c72f8d
Yusuke Izawa <me@yizawa.com>
2025-07-14 11:30:18 +0200

    Add main.jl

---
+++ src/main.jl
@@ -0,0 +1,17 @@
+include("./peval.jl")
+include("./jit.jl")
+
+function main(path)
+    # read file
+    f = open(path, 'r')
+    s = read(f, String)
+    ex = Meta.parse(s)
+
+    # partial the source program
+    func_expr, fname = partial_evaluate_and_make_entry(ex)
+
+    # compile code and run
+    # TODO: think about the way to integrate with notebook
+    res = compile_and_run(func_expr, fname, 123)
+    println(res)
+end
