73e2b7d75fd7ac69c30bceb18b03f0763c6e9724
Yusuke Izawa <me@yizawa.com>
2025-11-01 15:49:08 +0900

    Fix parse_annot

--- src/parse_annot.jl
+++ src/parse_annot.jl
@@ -8,6 +8,17 @@ function parse_annot(expr::Number, unconstant_expr)
     return expr
 end
 
+# Handle vectors (e.g., guard symbol payloads from split_ast)
+function parse_annot(expr::Vector, unconstant_expr)
+    for e in expr
+        if e isa Symbol
+            push!(unconstant_expr, e)
+        else
+            parse_annot(e, unconstant_expr)
+        end
+    end
+end
+
 function parse_annot(expr::Expr, unconstant_expr)
     if expr.head == :macrocall
         args = expr.args
@@ -19,16 +30,15 @@ function parse_annot(expr::Expr, unconstant_expr)
                 push!(unconstant_expr, args[2:end]...)
             end
         end
+    elseif expr.head == :hole
+        for arg in expr.args
+            if arg isa Symbol
+                push!(unconstant_expr, arg)
+            elseif arg isa Vector
+                parse_annot(arg, unconstant_expr)
+            end
+        end
     else
         [parse_annot(e, unconstant_expr) for e in expr.args]
     end
 end
-
-using Test
-
-# unconstant_expr = []
-# parse_annot(quote
-# 	            @hole x = 1
-#                 @hole f()
-#             end, unconstant_expr)
-# println(unconstant_expr)
