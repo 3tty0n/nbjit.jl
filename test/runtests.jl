ca32addf749e6073f9aef8fe4ac5fd5bded09233
Yusuke Izawa <me@yizawa.com>
2025-11-03 17:06:43 +0900

    Update runtest.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,20 +4,16 @@ println("Running nbjit.jl Test Suite\n")
 println("=" ^ 70)
 
 @testset "nbjit.jl Test Suite" begin
-    @testset "Pure Cell Caching" begin
-        include("test_pure_cache.jl")
-    end
-
     @testset "Split JIT Compilation" begin
         include("test_split_jit.jl")
     end
 
-    @testset "@cache Macro" begin
-        include("test_cache_macro.jl")
-    end
-
     @testset "JIT Features" begin
         include("test_jit.jl")
+    end
+
+    @testset "Notebook simulation" begin
+        include("test_demo.jl")
     end
 end
 
